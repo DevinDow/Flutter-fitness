7476012c2701d266b2c7ad30c563d8acd0bcaa22
Devin Dow <DevinDow@gmail.com>
2023-08-28 13:53:06 -0700

    code layout fixed

--- lib/routine/play_routine.dart
+++ lib/routine/play_routine.dart
@@ -20,83 +20,83 @@ class PlayRoutine extends StatelessWidget {
     Task task = routine.tasks[0];
 
     return Scaffold(
-        appBar: AppBar(
-          title: Text(routine.name),
-        ),
-        body: SafeArea(
-          child: Column(
-            children: [
-              // Move Name
-              Text(
-                task.moveName,
-                style: textTheme.headlineLarge,
-              ),
+      appBar: AppBar(
+        title: Text(routine.name),
+      ),
+      body: SafeArea(
+        child: Column(
+          children: [
+            // Move Name
+            Text(
+              task.moveName,
+              style: textTheme.headlineLarge,
+            ),
 
-              // Instructions
-              Text(
-                task.instructions,
-                style: textTheme.titleMedium,
-              ),
+            // Instructions
+            Text(
+              task.instructions,
+              style: textTheme.titleMedium,
+            ),
 
-              // Canvas
-              Container(
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 40, vertical: 80),
-                child: Container(
-                  decoration: BoxDecoration(
-                      border: Border.all(color: Colors.blueAccent)),
-                  child: LayoutBuilder(
-                    builder: (_, constraints) => SizedBox(
-                      width: 300, //constraints.widthConstraints().maxWidth,
-                      height: 300, //constraints.heightConstraints().maxHeight,
-                      child: CustomPaint(
-                          painter: MovePainter(moveName: task.moveName)),
-                    ),
+            // Canvas
+            Container(
+              padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 80),
+              child: Container(
+                decoration:
+                    BoxDecoration(border: Border.all(color: Colors.blueAccent)),
+                child: LayoutBuilder(
+                  builder: (_, constraints) => SizedBox(
+                    width: 300, //constraints.widthConstraints().maxWidth,
+                    height: 300, //constraints.heightConstraints().maxHeight,
+                    child: CustomPaint(
+                        painter: MovePainter(moveName: task.moveName)),
                   ),
                 ),
               ),
+            ),
 
-              // Time
-              Text(
-                "1:00",
-                style: textTheme.headlineMedium,
-              ),
+            // Time
+            Text(
+              "1:00",
+              style: textTheme.headlineMedium,
+            ),
 
-              // Buttons
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  ElevatedButton(
-                    onPressed: () {},
-                    child: const Icon(Icons.skip_previous),
-                  ),
-                  ElevatedButton(
-                    onPressed: () {},
-                    child: const Icon(Icons.play_arrow),
-                  ),
-                  ElevatedButton(
-                    onPressed: () {},
-                    child: const Icon(Icons.skip_next),
-                  ),
-                ],
-              ),
+            // Buttons
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                ElevatedButton(
+                  onPressed: () {},
+                  child: const Icon(Icons.skip_previous),
+                ),
+                ElevatedButton(
+                  onPressed: () {},
+                  child: const Icon(Icons.play_arrow),
+                ),
+                ElevatedButton(
+                  onPressed: () {},
+                  child: const Icon(Icons.skip_next),
+                ),
+              ],
+            ),
 
-              // Next/Remaining
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text(
-                    "Next: Down Dog",
-                    style: textTheme.bodyLarge,
-                  ),
-                  Text(
-                    "10 more = 4 min",
-                    style: textTheme.bodyMedium,
-                  ),
-                ],
-              ),
-            ],
-          ),
-        ));
+            // Next/Remaining
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  "Next: Down Dog",
+                  style: textTheme.bodyLarge,
+                ),
+                Text(
+                  "10 more = 4 min",
+                  style: textTheme.bodyMedium,
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
