b394e0ca26219d0a5dcd756584ab6dde8a1b121a
Devin Dow <DevinDow@gmail.com>
2023-08-28 15:14:48 -0700

    add Torso.  add Angle, but commented out in favor of 'package:angles/angles.dart'

--- lib/pose/torso.dart
+++ lib/pose/torso.dart
@@ -1,7 +1,75 @@
+import 'package:angles/angles.dart' as angles;
+
 import '../util/point.dart';
 
 class Torso {
-  Point waist = Point();
+  // Fields
+  late final Point waist;
+  late final double lengthRatio;
 
-  Torso();
+  late final Point collar;
+  late final Point head;
+
+  /*Point[] points;
+
+  Point rShoulder;
+  Point lShoulder;
+
+  Point rHip;
+  Point lHip;
+
+  bool mat = false;*/
+
+  // Constants
+  static const double headSize = 10;
+  static const thickness = 10;
+  static const double length = 20;
+  static const double lengthWithHead = length + headSize;
+  //static const double distanceNeckToShoulder = thickness/2 + Arm.thickness/2;
+  //static const double distanceWaistToHip = Leg.thickness/2 - 1;
+
+  // Constructors
+  Torso({
+    double waistY = 0,
+    this.lengthRatio = 1.0,
+    required angles.Angle angle,
+    bool isShoulderProfile = false,
+    bool isHipsProfile = false,
+  }) {
+    // Waist
+    waist = Point(y: waistY);
+
+    // Collar
+    collar = Point(
+      x: length * lengthRatio * angle.cos,
+      y: waistY + length + lengthRatio * angle.sin,
+    );
+
+    // Head
+    double waistToHead = length * lengthRatio + thickness / 2 + headSize / 2;
+    head = Point(
+      x: waistToHead * angle.cos,
+      y: waistY + waistToHead * angle.sin,
+    );
+
+    /*
+    // Shoulders
+    if (isShoulderProfile) {
+      rShoulder = collar.clone();
+      lShoulder = collar.clone();
+    } else {
+      rShoulder = collar.offset(-distanceNeckToShoulder * angle.getSin(), distanceNeckToShoulder * angle.getCos());
+      lShoulder = collar.offset(distanceNeckToShoulder * angle.getSin(), -distanceNeckToShoulder * angle.getCos());
+    }
+
+    // Hips
+    if (isHipsProfile) {
+      rHip = waist.clone();
+      lHip = waist.clone();
+    } else {
+      rHip = waist.offset(-distanceWaistToHip * angle.getSin(), distanceWaistToHip * angle.getCos());
+      lHip = waist.offset(distanceWaistToHip * angle.getSin(), -distanceWaistToHip * angle.getCos());
+    }
+    */
+  }
 }
---
+++ lib/util/angle.dart
@@ -0,0 +1,40 @@
+/*import 'package:vector_math/vector_math.dart' as vector_math;
+import 'package:angles/angles.dart' as angles;
+
+import 'point.dart';
+
+class AngleUtil {
+
+class Angle {
+  Angle();
+
+  /*//const
+
+  late final double radians;
+
+  Angle({required this.radians});
+
+  Angle.fromDegrees({required int degrees}) {
+    radians = vector_math.radians(1.0 * degrees);
+  }
+
+  Angle.fromDyDx({required double dy, required double dx}) {
+    if (dy == 0 && dx == 0) {
+      throw Exception("Angle can't be found from 0/0");
+    }
+
+    radians = angles.Angle.atan2(dy, dx).radians;
+  }
+
+  Angle.fromPoints({required Point p1, required Point p2}) {
+    Angle.fromDyDx(dy: p2.y - p1.y, dx: p2.x - p1.x);
+  }
+
+  Angle.fromHeightsLength(
+      {required double height1,
+      required double height2,
+      required double length}) {
+    double dy = height2 - height1;
+    radians = angles.Angle.asin(dy / length).radians;
+  }
+}*/
