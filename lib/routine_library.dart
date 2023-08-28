e7dccf35532ad12aa2ba20d9bb53dd6aeda942d7
Devin Dow <DevinDow@gmail.com>
2023-08-28 06:06:41 -0700

    generateMorningYoga()

--- lib/routine_library.dart
+++ lib/routine_library.dart
@@ -10,25 +10,31 @@ class RoutineLibrary {
   }
 
   static void generate() {
-    generateTestRoutine();
-    add(Routine(
-        name: "Warm-up Test Routine 2",
-        description: "description of Warm-up Test Routine #2",
-        category: Categories.warmup));
-    add(Routine(
-        name: "Warm-up Test Routine 3",
-        description: "description of Warm-up Test Routine #3",
-        category: Categories.warmup));
+    generateTestRoutines();
+    generateMorningYoga();
+  }
+
+  static void generateTestRoutines() {
+    Routine routine = Routine(
+      name: "Test Routine",
+      description: "description of Warm-up Test Routine",
+      category: Categories.warmup,
+    );
+    routine.tasks.add(Task(
+        moveName: MoveLibrary.MOUNTAIN_POSE, moveSeconds: 10, restSeconds: 5));
+    add(routine);
+
     add(Routine(
         name: "Strength Test Routine",
         description: "description of Strength Test Routine",
         category: Categories.strength));
   }
 
-  static void generateTestRoutine() {
+  static void generateMorningYoga() {
     Routine routine = Routine(
-      name: "Test Routine",
-      description: "description of Warm-up Test Routine",
+      name: "Morning Yoga",
+      description:
+          "Yoga for getting going when stiff from inactivity.  Breathe with each movement.",
       category: Categories.warmup,
     );
     routine.tasks.add(Task(
